f55b1e0d6e670b41e4d0d15975e34a14f5699c44
thizthizzydizzy <thizthizzydizzy@gmail.com>
2021-08-31 17:47:16 -0400

    Moved FileReader initialization to MenuInit JSON will now check the first non-whitespace character for { before parsing the whole file

--- src/net/ncplanner/plannerator/MenuInit.java
+++ src/net/ncplanner/plannerator/MenuInit.java
@@ -9,13 +9,42 @@ import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import net.ncplanner.plannerator.multiblock.configuration.Configuration;
 import net.ncplanner.plannerator.multiblock.configuration.TextureManager;
 import net.ncplanner.plannerator.planner.Core;
 import net.ncplanner.plannerator.planner.Supplier;
 import net.ncplanner.plannerator.planner.Task;
-import net.ncplanner.plannerator.planner.menu.MenuMain;
+import net.ncplanner.plannerator.planner.file.FileReader;
+import net.ncplanner.plannerator.planner.file.FormatReader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF10Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF11Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF1Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF2Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF3Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF4Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF5Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF6Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF7Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF8Reader;
+import net.ncplanner.plannerator.planner.file.reader.NCPF9Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageMSR1Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageMSR2Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageMSR3Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageMSR4Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageMSR5Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageMSR6Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageSFR1Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageSFR2Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageSFR3Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageSFR4Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageSFR5Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulHellrageSFR6Reader;
+import net.ncplanner.plannerator.planner.file.reader.OverhaulNCConfigReader;
+import net.ncplanner.plannerator.planner.file.reader.UnderhaulHellrage1Reader;
+import net.ncplanner.plannerator.planner.file.reader.UnderhaulHellrage2Reader;
+import net.ncplanner.plannerator.planner.file.reader.UnderhaulNCConfigReader;
 import net.ncplanner.plannerator.planner.menu.component.ProgressBar;
 import net.ncplanner.plannerator.planner.module.FusionTestModule;
 import net.ncplanner.plannerator.planner.module.Module;
@@ -28,12 +57,52 @@ import net.ncplanner.plannerator.simplelibrary.config2.Config;
 import net.ncplanner.plannerator.simplelibrary.config2.ConfigList;
 public class MenuInit extends Form{
     private final Task init;
+    HashMap<String, Supplier<FormatReader>> readers = new HashMap<>();
+    ArrayList<String> readerNames = new ArrayList<>();
+    HashMap<String, Task> readerTasks = new HashMap<>();
+    {
+        addReader("NCPF11Reader", ()->{return new NCPF11Reader();});// .ncpf version 11
+        addReader("NCPF10Reader", ()->{return new NCPF10Reader();});// .ncpf version 10
+        addReader("NCPF9Reader", ()->{return new NCPF9Reader();});// .ncpf version 9
+        addReader("NCPF8Reader", ()->{return new NCPF8Reader();});// .ncpf version 8
+        addReader("NCPF7Reader", ()->{return new NCPF7Reader();});// .ncpf version 7
+        addReader("NCPF6Reader", ()->{return new NCPF6Reader();});// .ncpf version 6
+        addReader("NCPF5Reader", ()->{return new NCPF5Reader();});// .ncpf version 5
+        addReader("NCPF4Reader", ()->{return new NCPF4Reader();});// .ncpf version 4
+        addReader("NCPF3Reader", ()->{return new NCPF3Reader();});// .ncpf version 3
+        addReader("NCPF2Reader", ()->{return new NCPF2Reader();});// .ncpf version 2
+        addReader("NCPF1Reader", ()->{return new NCPF1Reader();});// .ncpf version 1
+        addReader("OverhaulHellrageSFR6Reader", ()->{return new OverhaulHellrageSFR6Reader();});// hellrage SFR .json 2.1.1-2.1.7 (present)
+        addReader("OverhaulHellrageSFR5Reader", ()->{return new OverhaulHellrageSFR5Reader();});// hellrage SFR .json 2.0.32-2.0.37
+        addReader("OverhaulHellrageSFR4Reader", ()->{return new OverhaulHellrageSFR4Reader();});// hellrage SFR .json 2.0.31
+        addReader("OverhaulHellrageSFR3Reader", ()->{return new OverhaulHellrageSFR3Reader();});// hellrage SFR .json 2.0.30
+        addReader("OverhaulHellrageSFR2Reader", ()->{return new OverhaulHellrageSFR2Reader();});// hellrage SFR .json 2.0.7-2.0.29
+        addReader("OverhaulHellrageSFR1Reader", ()->{return new OverhaulHellrageSFR1Reader();});// hellrage SFR .json 2.0.1-2.0.6
+        addReader("UnderhaulHellrage2Reader", ()->{return new UnderhaulHellrage2Reader();});// hellrage .json 1.2.23-1.2.25 (present)
+        addReader("UnderhaulHellrage1Reader", ()->{return new UnderhaulHellrage1Reader();});// hellrage .json 1.2.5-1.2.22
+        addReader("OverhaulHellrageMSR6Reader", ()->{return new OverhaulHellrageMSR6Reader();});// hellrage MSR .json 2.1.1-2.1.7 (present)
+        addReader("OverhaulHellrageMSR5Reader", ()->{return new OverhaulHellrageMSR5Reader();});// hellrage MSR .json 2.0.32-2.0.37
+        addReader("OverhaulHellrageMSR4Reader", ()->{return new OverhaulHellrageMSR4Reader();});// hellrage MSR .json 2.0.31
+        addReader("OverhaulHellrageMSR3Reader", ()->{return new OverhaulHellrageMSR3Reader();});// hellrage MSR .json 2.0.30
+        addReader("OverhaulHellrageMSR2Reader", ()->{return new OverhaulHellrageMSR2Reader();});// hellrage MSR .json 2.0.7-2.0.29
+        addReader("OverhaulHellrageMSR1Reader", ()->{return new OverhaulHellrageMSR1Reader();});// hellrage MSR .json 2.0.1-2.0.6
+        addReader("OverhaulNCConfigReader", ()->{return new OverhaulNCConfigReader();});// OVERHAUL nuclearcraft.cfg
+        addReader("UnderhaulNCConfigReader", ()->{return new UnderhaulNCConfigReader();});// UNDERHAUL nuclearcraft.cfg
+    }
+    private  void addReader(String s, Supplier<FormatReader> reader){
+        readerNames.add(s);
+        readers.put(s, reader);
+    }
     public MenuInit(Supplier<Form> formToShow){
         super(new BorderLayout());
         init = new Task("Initializing...");
         Task t1 = init.addSubtask("Creating form...");
         Task ts = init.addSubtask("Loading settings...");
         Task t2 = init.addSubtask("Resetting Metadata");
+        Task tf = init.addSubtask("Adding File Readers...");
+        for(String s : readerNames){
+            readerTasks.put(s, tf.addSubtask("Adding "+s+"..."));
+        }
         Task tc = init.addSubtask("Initializing Configurations...");
         Task tc1 = tc.addSubtask("Initializing Nuclearcraft Configuration");
         Task tcc = tc.addSubtask("Initializing Core configuration");
@@ -104,6 +173,11 @@ public class MenuInit extends Form{
             Core.resetMetadata();
             t2.finish();
             repaint();
+            for(String s : readerNames){
+                FileReader.formats.add(readers.get(s).get());
+                readerTasks.get(s).finish();
+                repaint();
+            }
             Configuration.initNuclearcraftConfiguration();
             tc1.finish();
             repaint();
